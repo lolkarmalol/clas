a4732b3b38b3291be657f4a9855bb68f94f7abef
Jodier Guzman Zapata <129127785+lolkarmalol@users.noreply.github.com>
2024-12-05 11:18:51 -0500

    .

---
+++ src/models/Categoria.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Categoria {
+    private final int id; // Cambiado a final
+    private String nombre;
+
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
---
+++ src/models/Usuario.java
@@ -0,0 +1,45 @@
+package models;
+
+public class Usuario {
+    private String nombre;
+    private String contraseña;
+    private String rol;
+
+    public Usuario(String nombre, String contraseña, String rol) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", rol='" + rol + '\'' +
+                '}';
+    }
+}
